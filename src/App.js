4bce86eb7adcabc60822cc3d02237a5b1d3d8087
Cleanly <oskar.turesson99@gmail.com>
2020-05-04 22:33:55 +0200

    updated path

--- src/App.js
+++ src/App.js
@@ -84,7 +84,7 @@ function App() {
 
         <Router>
           <Home path="/" events={events} />
-          <SingleEvent path="/event/*" re={userMessage} />
+          <SingleEvent path="*/event/*" re={userMessage} />
         </Router>
       </div>
     </div>
